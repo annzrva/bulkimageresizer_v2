2d342c8ae49b98df45be08f974cb9250ba2b7b3a
annzrva <133603057+annzrva@users.noreply.github.com>
2025-01-16 22:09:38 +0300

    Use default Next.js output directory

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,6 @@ const nextConfig: NextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/bulkimageresizer_v2' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/bulkimageresizer_v2' : '',
   trailingSlash: true,
-  distDir: 'dist',
   reactStrictMode: true,
   poweredByHeader: false,
 } as const;
